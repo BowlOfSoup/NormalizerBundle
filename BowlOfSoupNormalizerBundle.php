91114c58c59d60d4dcfb11a37c5d68f2a7ea183e
BowlOfSoup <BowlOfSoup@users.noreply.github.com>
2016-05-23 00:06:18 +0200

    Removed unused use-statement.

--- BowlOfSoupNormalizerBundle.php
+++ BowlOfSoupNormalizerBundle.php
@@ -2,7 +2,6 @@
 
 namespace BowlOfSoup\NormalizerBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class BowlOfSoupNormalizerBundle extends Bundle
