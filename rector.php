937165445abbdbe43bc8cd9105b08e1954a75668
Gábor Barnóth <19374371+gabriel-255@users.noreply.github.com>
2023-11-03 14:57:52 +0100

    Add Rector dev-dependency and configuration

---
+++ rector.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Symfony\Set\SymfonyLevelSetList;
+
+return static function (RectorConfig $rectorConfig): void
+{
+    $rectorConfig->paths([
+        __DIR__,
+    ]);
+
+    $rectorConfig->skip([
+        __DIR__ . '/var',
+        __DIR__ . '/vendor',
+    ]);
+
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_72,
+        SymfonyLevelSetList::UP_TO_SYMFONY_54,
+    ]);
+
+    $rectorConfig->importNames(true, false);
+    $rectorConfig->importShortClasses(false);
+
+    $rectorConfig->cacheDirectory(__DIR__ . '/var/cache/rector');
+    $rectorConfig->containerCacheDirectory(__DIR__ . '/var');
+};
